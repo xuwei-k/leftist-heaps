069e58916a6589f6fe660ac539a46bba57078b64
xuwei-k <6b656e6a69@gmail.com>
2012-12-07 17:43:37 +0900

    fix compile error

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,8 +5,8 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val list = List(1, 2, 3, 4, 5, 6, 7, 8)
-    val h1 = Heap.merge(Empty(), Empty())
-    val h2 = Heap.insert(1, Empty())
+    val h1 = Heap.merge(Empty, Empty)
+    val h2 = Heap.insert(1, Empty)
     val h3 = Heap.listToHeap(list)
     println("hello")
   }
--- src/main/scala/com/qtamaki/lh/Heap.scala
+++ src/main/scala/com/qtamaki/lh/Heap.scala
@@ -2,13 +2,13 @@ package com.qtamaki.lh
 
 import scala.math.Ordered._
 
-sealed abstract class Heap[T]
-case class Empty[T <% Ordered[T]] extends Heap[T]
+sealed abstract class Heap[+T]
+case object Empty extends Heap[Nothing]
 case class Tree[T <% Ordered[T]](rank: Int, node: T, left: Heap[T], right: Heap[T]) extends Heap[T]
 
 object Heap {
   private def rank[T](h: Heap[T]): Int = h match {
-    case Empty() => 0
+    case Empty => 0
     case Tree(x, _, _, _) => x
     case _ => throw new Exception()
   }
@@ -18,8 +18,8 @@ object Heap {
     Tree(rank(h1) + 1, node, h2, h1)
   }
   def merge[T <% Ordered[T]](heap1: Heap[T], heap2: Heap[T]): Heap[T] = (heap1, heap2) match {
-    case (Empty(), h) => h
-    case (h, Empty()) => h
+    case (Empty, h) => h
+    case (h, Empty) => h
     //    case (h1 @ Tree(_, x, a1, b1), h2 @ Tree(_, y, a2, b2)) if x < y => makeTree(x, a1, merge(b1, h2))
     //    case (h1 @ Tree(_, x, a1, b1), h2 @ Tree(_, y, a2, b2)) => makeTree(x, a2, merge(b2, h1))
     case (h1: Tree[T], h2: Tree[T]) => {
@@ -32,7 +32,7 @@ object Heap {
     case _ => throw new Exception()
   }
   def insert[T<% Ordered[T]](node: T, heap: Heap[T]): Heap[T] = {
-    merge(Tree(1, node, Empty(), Empty()), heap)
+    merge(Tree(1, node, Empty, Empty), heap)
   }
   def findMin[T<% Ordered[T]](h: Heap[T]): T = h match {
     case t: Tree[T] => t.node
@@ -42,9 +42,9 @@ object Heap {
     case Tree(_, x, a, b) => merge(a, b)
     case _ => throw new Exception()
   }
-  def listToHeap[T<% Ordered[T]](xs: List[T]) = xs.foldRight[Heap[T]](Empty())(insert _)
+  def listToHeap[T<% Ordered[T]](xs: List[T]) = xs.foldRight[Heap[T]](Empty)(insert _)
   def heapToList[T<% Ordered[T]](h: Heap[T]): List[T] = h match {
-    case Empty() => List()
+    case Empty => List()
     //case t:Tree[T] => f(implicit c:ClassManifest[T])indMin(t) :: heapToList(deleteMin(t))
     case t: Tree[T] => {
       val n: T = findMin(t)
